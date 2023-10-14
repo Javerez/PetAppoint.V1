68fb2ba0c7a36364b8f76e2de6e23c753aac8a11
Lucas042002 <lucaschavez2002@gmail.com>
2023-10-14 17:17:03 -0300

    Agregado el Inicio Sesion
    Faltaria el diseño de la pagina

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { NgxCaptchaModule } from 'ngx-captcha';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +22,11 @@ import { RegistroUsuarioComponent } from './componenetes/registro-usuario/regist
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgxCaptchaModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- FrontEnd/src/app/componenetes/inicio-sesion/inicio-sesion.component.ts
+++ FrontEnd/src/app/componenetes/inicio-sesion/inicio-sesion.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Router } from '@angular/router'
+//import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-inicio-sesion',
@@ -7,4 +10,61 @@ import { Component } from '@angular/core';
 })
 export class InicioSesionComponent {
 
+  formInicioSesion!: FormGroup;
+  buttonClicked!: boolean;
+  public captchaResolved : boolean = false;
+  public siteKey : any;
+  error_id:any;
+
+  constructor(
+    private formBuilder: FormBuilder, 
+    private router: Router ,
+    //private usuarioService:UsuarioService
+  ){}
+
+  ngOnInit(): void {
+    this.buttonClicked=false;
+    this.captchaResolved=false;
+    
+    let formulario = {
+      email: ['', Validators.compose([
+        Validators.required,
+        Validators.email
+      ])],
+      contrasenya: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(/^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,}$/)
+      ])],
+      recaptcha: ['',Validators.required]
+    }
+    this.formInicioSesion = this.formBuilder.group(formulario);
+    this.siteKey = "6LcCcJkmAAAAAM8lZ_jL7MZeSOI1iKd4exAu2wI1";
+
+  }
+  checkCaptcha(captchaResponse : string) {    
+    this.captchaResolved = (captchaResponse && captchaResponse.length > 0) ? true : false
+  }
+  
+  iniciarSesion() {
+    if (this.formInicioSesion.status === 'VALID') {
+      /*this.usuarioService.inicioSesionUsuario(this.formInicioSesion.value).subscribe(data => {
+          console.log(data.id)
+          switch(data.id){
+              case 1:
+                this.error_id=data.id;
+                break;
+              case 2:
+                this.error_id=data.id;
+                break;
+              case 3:
+                localStorage.setItem('token',data.token);
+                localStorage.setItem('userData',JSON.stringify(data.resultados[0]));
+                this.router.navigate(['perfil']);
+                break;
+          }
+          
+      });*/
+      console.log(this.formInicioSesion.value)
+    }
+  }
 }
