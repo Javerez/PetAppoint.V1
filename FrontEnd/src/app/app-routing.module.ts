fbde3c2a6250822950b3e3a3a144038c1295f14c
Lucas042002 <lucaschavez2002@gmail.com>
2023-10-14 16:17:03 -0300

    Cambios Routing module

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioSesionComponent } from './componenetes/inicio-sesion/inicio-sesion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",pathMatch:"full",redirectTo:"inicio-sesion"},
+  {path:"inicio-sesion",component:InicioSesionComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
