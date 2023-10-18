796d7c5e97107317c94ac6f12a07e6ce461abb1f
Javerez <javierperez0108@gmail.com>
2023-10-18 00:34:25 -0300

    BD actualizada
    agregado de columnas y cambios en valores

--- BD/petappoint.sql
+++ BD/petappoint.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-06-2023 a las 21:57:48
+-- Tiempo de generación: 18-10-2023 a las 05:33:30
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -28,10 +28,23 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `consulta` (
-  `IdConsulta` int(4) NOT NULL,
-  `Fecha` date NOT NULL,
-  `NombreAnimal` varchar(100) NOT NULL
+  `idConsulta` int(4) NOT NULL,
+  `fecha` date NOT NULL,
+  `nombreAnimal` varchar(100) NOT NULL,
+  `emailVet` varchar(100) NOT NULL,
+  `emailCliente` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `consulta`
+--
+
+INSERT INTO `consulta` (`idConsulta`, `fecha`, `nombreAnimal`, `emailVet`, `emailCliente`) VALUES
+(1, '2023-08-01', 'lucas', '', ''),
+(2, '2023-09-18', 'choripan', '', ''),
+(3, '2023-08-07', 'andre', '', ''),
+(4, '2023-10-02', 'zapallo', '', ''),
+(5, '2023-11-09', 'anticucho', '', '');
 
 -- --------------------------------------------------------
 
@@ -40,8 +53,8 @@ CREATE TABLE `consulta` (
 --
 
 CREATE TABLE `receta` (
-  `IdReceta` int(4) NOT NULL,
-  `IdConsulta` int(4) NOT NULL
+  `idReceta` int(4) NOT NULL,
+  `idConsulta` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -51,12 +64,19 @@ CREATE TABLE `receta` (
 --
 
 CREATE TABLE `usuario` (
-  `Nombre` varchar(100) NOT NULL,
-  `Apellido` varchar(100) NOT NULL,
-  `Email` varchar(100) NOT NULL,
-  `Password` int(100) NOT NULL,
-  `Admin` int(2) NOT NULL
+  `nombre` varchar(100) NOT NULL,
+  `apellido` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `admin` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`nombre`, `apellido`, `email`, `password`, `admin`) VALUES
+('kevo', 'las', 'kevolas@gmail.com', 'U2FsdGVkX18m97lCqxlDmN87kkUgwJz0t5ejxztA4Dw=', 1);
 
 --
 -- Índices para tablas volcadas
@@ -66,20 +86,21 @@ CREATE TABLE `usuario` (
 -- Indices de la tabla `consulta`
 --
 ALTER TABLE `consulta`
-  ADD PRIMARY KEY (`IdConsulta`);
+  ADD PRIMARY KEY (`idConsulta`),
+  ADD KEY `email` (`emailVet`);
 
 --
 -- Indices de la tabla `receta`
 --
 ALTER TABLE `receta`
-  ADD PRIMARY KEY (`IdReceta`),
-  ADD KEY `IdConsulta` (`IdConsulta`);
+  ADD PRIMARY KEY (`idReceta`),
+  ADD KEY `IdConsulta` (`idConsulta`);
 
 --
 -- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`Email`);
+  ADD PRIMARY KEY (`email`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -89,13 +110,13 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `consulta`
 --
 ALTER TABLE `consulta`
-  MODIFY `IdConsulta` int(4) NOT NULL AUTO_INCREMENT;
+  MODIFY `idConsulta` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `receta`
 --
 ALTER TABLE `receta`
-  MODIFY `IdReceta` int(4) NOT NULL AUTO_INCREMENT;
+  MODIFY `idReceta` int(4) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
@@ -105,7 +126,7 @@ ALTER TABLE `receta`
 -- Filtros para la tabla `receta`
 --
 ALTER TABLE `receta`
-  ADD CONSTRAINT `receta_ibfk_1` FOREIGN KEY (`IdConsulta`) REFERENCES `consulta` (`IdConsulta`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `receta_ibfk_1` FOREIGN KEY (`IdConsulta`) REFERENCES `consulta` (`idConsulta`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
